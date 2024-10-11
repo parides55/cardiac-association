26c0ecdb950de4552221923454ef9e49fdc9fdc2
parides55 <andreasparides5@gmail.com>
2024-10-11 22:26:13 +0100

    connect the supabase database and create a superuser

--- cardiac_association/settings.py
+++ cardiac_association/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 import sys
+import dj_database_url
 from django.contrib.messages import constants as messages
 if os.path.isfile('env.py'):
     import env
@@ -31,7 +32,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 SECRET_KEY = 'django-insecure-nibnw@m&6%3t-x%)$d$=#ez^eqai6)-x80r2g99fjll8jmh^xk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com', '*']
 
@@ -87,10 +88,7 @@ WSGI_APPLICATION = 'cardiac_association.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
