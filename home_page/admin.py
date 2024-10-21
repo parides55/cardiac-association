8dd8018f26b0d2bd2c9de1165f84e9f86b9ec459
parides55 <andreasparides5@gmail.com>
2024-10-21 23:53:49 +0100

    create a HeroImage model in the homepage and add it in the admin panel

--- home_page/admin.py
+++ home_page/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import HeroImage
 
 # Register your models here.
+admin.site.register(HeroImage)
--- home_page/models.py
+++ home_page/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+class HeroImage(models.Model):
+    image = CloudinaryField('image', default='placeholder')
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100, unique=True)
+    description = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title, self.created_on
+    
+    class Meta:
+        ordering = ['-created_on']
