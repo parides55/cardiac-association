39d0ff5121034bae5d47d99857729c6f466259b4
parides55 <andreasparides5@gmail.com>
2024-10-22 20:22:16 +0100

    create HeroImage model in the home_page, add summernote to admin panel and migrate it

--- home_page/admin.py
+++ home_page/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from .models import HeroImage
 
+@admin.register(HeroImage)
+class HeroImageAdmin(admin.ModelAdmin):
+
+    list_display = ['title', 'created_on', 'active']
+    list_filter = ['created_on', 'active']
+    search_fields = ['title', 'created_on']
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('description',)
+
+
 # Register your models here.
-admin.site.register(HeroImage)
--- home_page/models.py
+++ home_page/models.py
@@ -3,11 +3,13 @@ from cloudinary.models import CloudinaryField
 
 # Create your models here.
 class HeroImage(models.Model):
+
     image = CloudinaryField('image', default='placeholder')
     title = models.CharField(max_length=100)
     slug = models.SlugField(max_length=100, unique=True)
     description = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=True)
 
     def __str__(self):
         return self.title, self.created_on
