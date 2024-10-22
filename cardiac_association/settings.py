1c51b79bd0356ae03ec985ff63831dca46156262
parides55 <andreasparides5@gmail.com>
2024-10-22 22:40:12 +0100

    update the secret key and place it in the env.py

--- cardiac_association/settings.py
+++ cardiac_association/settings.py
@@ -29,7 +29,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nibnw@m&6%3t-x%)$d$=#ez^eqai6)-x80r2g99fjll8jmh^xk'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
