3bee70f12a11bb0af66ffbdc51f57d922734ba22
parides55 <andreasparides5@gmail.com>
2024-11-07 14:08:48 +0000

    create the model for storing news, customize the admin panel and make migrations

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
+from .models import News
+
+
+@admin.register(News)
+class NewAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'created_on', 'status')
+    list_filter = ('created_on', 'status')
+    search_fields = ['title', 'created_on']
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
+
 
 # Register your models here.
--- news/models.py
+++ news/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
+
+STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
+class News(models.Model):
+    
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    image = CloudinaryField('image', default='placeholder')
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    excerpt = models.TextField(blank=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    
+    def __str__(self):
+        return f"{self.title} - {self.created_on.strftime('%Y-%m-%d')}"
+    
+    class Meta:
+        ordering = ['-created_on']
