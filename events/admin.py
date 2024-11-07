bc6502f8445ffe964b22e096eadc7a5e80c07256
parides55 <andreasparides5@gmail.com>
2024-11-07 23:42:20 +0000

    create the events and news models, customize the admin panel with them and make migrations

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
+from .models import Event
+
+
+@admin.register(Event)
+class EventAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'created_on', 'status')
+    list_filter = ('created_on', 'status')
+    search_fields = ['title', 'created_on']
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
 
 # Register your models here.
