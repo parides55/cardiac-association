30a3986b76540989f3a734a9c879a42b2ff53bab
parides55 <andreasparides5@gmail.com>
2024-11-08 11:43:56 +0000

    delete the model and admin panel for the homepage

--- news/admin.py
+++ news/admin.py
@@ -1,16 +1,3 @@
 from django.contrib import admin
-from django_summernote.admin import SummernoteModelAdmin
-from .models import New
-
-
-@admin.register(New)
-class NewAdmin(SummernoteModelAdmin):
-
-    list_display = ('title', 'created_on', 'status')
-    list_filter = ('created_on', 'status')
-    search_fields = ['title', 'created_on']
-    prepopulated_fields = {'slug': ('title',)}
-    summernote_fields = ('content',)
-
 
 # Register your models here.
