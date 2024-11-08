b387572cf42c1fc4c1da34203daedaddb8049c4c
parides55 <andreasparides5@gmail.com>
2024-11-08 11:48:47 +0000

    delete the model and admin panel for the homepage

--- home_page/admin.py
+++ home_page/admin.py
@@ -1,15 +1,3 @@
 from django.contrib import admin
-from django_summernote.admin import SummernoteModelAdmin
-from .models import HeroImage
-
-@admin.register(HeroImage)
-class HeroImageAdmin(SummernoteModelAdmin):
-
-    list_display = ('title', 'created_on', 'active')
-    list_filter = ('created_on', 'active')
-    search_fields = ['title', 'created_on']
-    prepopulated_fields = {'slug': ('title',)}
-    summernote_fields = ('description',)
-
 
 # Register your models here.
--- home_page/models.py
+++ home_page/models.py
@@ -1,18 +1,3 @@
 from django.db import models
-from cloudinary.models import CloudinaryField
 
 # Create your models here.
-class HeroImage(models.Model):
-
-    image = CloudinaryField('image', default='placeholder')
-    title = models.CharField(max_length=100)
-    slug = models.SlugField(max_length=100, unique=True)
-    description = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)
-    active = models.BooleanField(default=False)
-
-    def __str__(self):
-        return f"{self.title} - {self.created_on.strftime('%Y-%m-%d')}"
-    
-    class Meta:
-        ordering = ['-created_on']
