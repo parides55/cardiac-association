77a01eab752dc312812db26012e624d6d9a6def3
parides55 <andreasparides5@gmail.com>
2024-11-11 10:46:38 +0000

    create and connect the services.html and programs.html into what we do page. create the initial styles and layout

--- what_we_do/urls.py
+++ what_we_do/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.WhatWeDo, name="what_we_do"),
+    path("services/", views.Services, name="services"),
+    path("programs/", views.Programs, name="programs"),
 ]
--- what_we_do/views.py
+++ what_we_do/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def WhatWeDo(request):
     return render(request, "what_we_do/what_we_do.html")
+
+def Services(request):
+    return render(request, "what_we_do/services.html")
+
+def Programs(request):
+    return render(request, "what_we_do/programs.html")
