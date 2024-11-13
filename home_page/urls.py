5844bd8673b0e584d478ac3c39fa48e00828d273
parides55 <andreasparides5@gmail.com>
2024-11-13 11:30:02 +0000

    create the donations and become a member pages and add their links in all necessary pages

--- home_page/urls.py
+++ home_page/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="home"),
     path("heart_disease_info/", views.MoreInfo, name="heart_disease_info"),
+    path("become_member/", views.Become_member, name="become_member"),
+    path("donations/", views.Donations, name="donations"),
 ]
--- home_page/views.py
+++ home_page/views.py
@@ -6,3 +6,9 @@ def index(request):
 
 def MoreInfo(request):
     return render(request, "home_page/heart_disease_info.html")
+
+def Become_member(request):
+    return render(request, "home_page/become_member.html")
+
+def Donations(request):
+    return render(request, "home_page/donations.html")
