1a2491f65637b6769efca6032b54a9a84dc79d7c
parides55 <andreasparides5@gmail.com>
2024-11-13 13:06:14 +0000

    correct the forms name in the BecomeMember function

--- home_page/views.py
+++ home_page/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .forms import ContactForm
+from .forms import MemberForm
 
 
 def index(request):
@@ -9,10 +9,10 @@ def MoreInfo(request):
     return render(request, "home_page/heart_disease_info.html")
 
 def Become_member(request):
-    comment_form = ContactForm()
+    member_form = MemberForm()
     return render(request,
                 "home_page/become_member.html"
-                , {'comment_form': comment_form})
+                , {'member_form': member_form})
 
 def Donations(request):
     return render(request, "home_page/donations.html")
