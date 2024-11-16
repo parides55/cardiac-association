dafc2eaf41f950021ea57b114ce676d7a82f3640
parides55 <andreasparides5@gmail.com>
2024-11-16 15:03:36 +0000

    create the sections to display this months, upcoming and past events. add styles to make it responsive and buttons to view events

--- events/views.py
+++ events/views.py
@@ -4,6 +4,7 @@ from .models import Event
 
 # Create your views here.
 def events(request):
+    today = datetime.now()
     current_month = datetime.now().month
     current_year = datetime.now().year
     
@@ -11,16 +12,24 @@ def events(request):
         date_of_event__month=current_month,
         date_of_event__year=current_year,
         status=1
-        )
+        ).order_by('date_of_event')
     
-    past_events = Event.objects.exclude(
+    upcoming_events = Event.objects.filter(
+        date_of_event__gt=today,
+        status=1
+    ).exclude(
         date_of_event__month=current_month,
-        date_of_event__year=current_year,
-        status=0
-    )
+        date_of_event__year=current_year
+    ).order_by('date_of_event')
+    
+    past_events = Event.objects.filter(
+        date_of_event__lt=today,
+        status=1
+    ).order_by('-date_of_event') 
     
     context = {
         'current_month_events': current_month_events,
+        'upcoming_events': upcoming_events,
         'past_events': past_events,
     }
     
