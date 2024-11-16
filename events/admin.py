c43dee28a4f3e2378445aa1fffd63b88a6981e98
parides55 <andreasparides5@gmail.com>
2024-11-16 13:13:53 +0000

    add the date of event to display in the admin panel

--- events/admin.py
+++ events/admin.py
@@ -6,7 +6,7 @@ from .models import Event
 @admin.register(Event)
 class EventAdmin(SummernoteModelAdmin):
 
-    list_display = ('title', 'created_on', 'status')
+    list_display = ('title', 'created_on', 'date_of_event', 'status')
     list_filter = ('created_on', 'status')
     search_fields = ['title', 'created_on']
     prepopulated_fields = {'slug': ('title',)}
