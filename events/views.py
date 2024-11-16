4f4b12a247c3a01f2b1675922870462d37ba0069
parides55 <andreasparides5@gmail.com>
2024-11-16 12:19:01 +0000

    add code to search for current and past events int he Events model and display them in the template

--- events/views.py
+++ events/views.py
@@ -1,5 +1,27 @@
 from django.shortcuts import render
+from datetime import datetime
+from .models import Event
 
 # Create your views here.
 def events(request):
-    return render(request, "events/events.html")
+    current_month = datetime.now().month
+    current_year = datetime.now().year
+    
+    current_month_events = Event.objects.filter(
+        date__month=current_month,
+        date__year=current_year,
+        status=1
+        )
+    
+    past_events = Event.objects.exclude(
+        date__month=current_month,
+        date__year=current_year,
+        status=0
+    )
+    
+    context = {
+        'current_month_events': current_month_events,
+        'past_events': past_events,
+    }
+    
+    return render(request, "events/events.html", context)
