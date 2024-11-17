839f946d19c15f1429e177f6893cb8105f8ce2e8
parides55 <andreasparides5@gmail.com>
2024-11-17 12:42:24 +0000

    create people's model, register it in admin panel and migrate it. add a default profile pic

--- about_us/models.py
+++ about_us/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+POSITIONS = ((0, "Chairman"), (1, "Vice Chairman"), (2, "Secretary"), (3, "Assistant Secretary"),
+            (4, "Treasurer"), (5,"Assistant Treasurer"), (6, "Member"), (7, "Staff"))
+
+
+class People(models.Model):
+    name = models.CharField(max_length=100)
+    image = CloudinaryField('image', default='placeholder')
+    POSITIONS = models.IntegerField(choices=POSITIONS, default=0)
