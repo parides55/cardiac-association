96d503ba7bcd67b838cf8db0c9619b64b2c42fda
parides55 <andreasparides5@gmail.com>
2025-01-02 23:04:07 +0000

    update the members forms to apply translations

--- home_page/forms.py
+++ home_page/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.utils.translation import gettext_lazy as _
 from .models import Member
 
 class MemberForm(forms.ModelForm):
@@ -25,5 +26,17 @@ class MemberForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['date_of_diagnosis'].label = "Date of Diagnosis"
-        self.fields['registered_as'].label = "Registered_as"
+        self.fields['registered_as'].label = _("Registered As")
+        self.fields['name'].label = _("Name")
+        self.fields['surname'].label = _("Surname")
+        self.fields['email'].label = _("Email")
+        self.fields['id_number'].label = _("ID Number")
+        self.fields['profession'].label = _("Profession")
+        self.fields['home_number'].label = _("Home Number")
+        self.fields['mobile_number'].label = _("Mobile Number")
+        self.fields['work_number'].label = _("Work Number")
+        self.fields['date_of_birth'].label = _("Date of Birth")
+        self.fields['heart_disease_description'].label = _(
+            "Heart Disease Description (if not a sufferer, please write N/A)"
+        )
+        self.fields['date_of_diagnosis'].label = _("Date of Diagnosis")
