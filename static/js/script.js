c3c9dfaaa17e9a73a111ca0c4dce0a55a1241329
parides55 <andreasparides5@gmail.com>
2025-01-28 00:20:00 +0700

    add code to remove the hide class and add the animated effects

--- static/js/script.js
+++ static/js/script.js
@@ -4,12 +4,15 @@ const ctaButtonHome = document.querySelector('.cta-btn-home');
 
 window.onload = () => {
     if (downArrow) {
+        downArrow.classList.remove('hide');
         downArrow.classList.add('animate__animated', 'animate__fadeInDown', 'animate__slower', 'animate__delay-1s');
     }
     if (heroHeading) {
+        heroHeading.classList.remove('hide');
         heroHeading.classList.add('animate__animated', 'animate__fadeIn', 'animate__slow', 'animate__delay-1s');
     }
     if (ctaButtonHome) {
+        ctaButtonHome.classList.remove('hide');
         ctaButtonHome.classList.add('animate__animated', 'animate__fadeInLeft', 'animate__slow', 'animate__delay-1s');
     }
 };
