e68370a674be333acfd868c2b72b2b95df29473e
parides55 <parides5@hotmail.com>
2025-02-06 04:42:41 +0800

    update basket.js

--- static/js/basket.js
+++ static/js/basket.js
@@ -4,11 +4,14 @@ const removeButtons = document.getElementsByClassName("btn-to-remove-item");
 const removeConfirm = document.getElementById("removeConfirm");
 
 // Gets the item's id to be removed from the basket and initiates the removal modal
-for (let button of removeButtons) {
-    button.addEventListener('click', (e) => {
-        let itemId = e.target.getAttribute('data-basket_id');
-        removeConfirm.href = `remove_from_basket/${itemId}`;
-        removalModal.show();
+if (removalModal && removeConfirm) {
+    document.addEventListener('click', (e) => {
+        if (e.target.classList.contains('btn-to-remove-item')) {
+            e.preventDefault();
+            let itemId = e.target.getAttribute('data-basket_id');
+            removeConfirm.href = `remove_from_basket/${itemId}`;
+            removalModal.show();
+            removeConfirm.focus();
+        }
     });
-    
 }
