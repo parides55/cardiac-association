7b1c3de73b908a2edb8b8795218e613469c19e23
parides55 <andreasparides5@gmail.com>
2025-02-07 13:53:30 +0000

    display the ID of the member

--- home_page/admin.py
+++ home_page/admin.py
@@ -6,7 +6,7 @@ from .models import Member
 @admin.register(Member)
 class MemberAdmin(SummernoteModelAdmin):
     
-    list_display = ('name', 'surname', 'date_of_birth', 'created_at')
+    list_display = ('id', 'name', 'surname', 'date_of_birth', 'created_at')
     list_filter = ('date_of_birth', 'name', 'surname',)
     search_fields = ['name', 'surname', 'email', 'id_number', 'date_of_birth']
 
