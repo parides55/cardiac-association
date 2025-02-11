860e36423a770e18dc1e30982953e3aafe963fa7
parides55 <andreasparides5@gmail.com>
2025-02-11 22:58:59 +0000

    comment put the process payment

--- home_page/views.py
+++ home_page/views.py
@@ -77,24 +77,24 @@ def Become_member(request):
             print(member_form.errors)
             if member_form.is_valid():
                 member_form.save()
-                # messages.success(request, f"Welcome to the family of the Association of Children with Heart Disease." 
-                #     f"Your membership has been successfully registered.")
-                # return render(request, "home_page/index.html")
+                messages.success(request, f"Welcome to the family of the Association of Children with Heart Disease." 
+                    f"Your membership has been successfully registered.")
+                return render(request, "home_page/index.html")
                 
-                # Process payment
-                try:
-                    payment_url = process_payment(member_form.instance.id)
-                    return redirect(payment_url) # Redirect user to JCC payment page
-                except Exception as e:
-                    messages.error(request, f"An error occurred while processing your payment: {str(e)}")
-                    return redirect('home_page/index.html')
+                # # Process payment
+                # try:
+                #     payment_url = process_payment(member_form.instance.id)
+                #     return redirect(payment_url) # Redirect user to JCC payment page
+                # except Exception as e:
+                #     messages.error(request, f"An error occurred while processing your payment: {str(e)}")
+                #     return redirect('home_page/index.html')
             else:
                 messages.error(
                     request,
                     f"There has been error processing your request. Please try completing "
                     f"the form again."
                 )
-                return redirect('become_member')
+                return render(request, "home_page/index.html")
 
         member_form = MemberForm()
 
