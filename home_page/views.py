d7c63dde6def5e643e74aaaa764b6187242684c8
parides55 <andreasparides5@gmail.com>
2025-02-11 09:19:14 +0000

    comment out the process payment

--- home_page/views.py
+++ home_page/views.py
@@ -76,17 +76,17 @@ def Become_member(request):
             member_form = MemberForm(request.POST)
             if member_form.is_valid():
                 member_form.save()
-                # messages.success(request, f"Welcome to the family of the Association of Children with Heart Disease." 
-                #     f"Your membership has been successfully registered.")
-                # return render(request, "home_page/index.html")
+                messages.success(request, f"Welcome to the family of the Association of Children with Heart Disease." 
+                    f"Your membership has been successfully registered.")
+                return render(request, "home_page/index.html")
                 
                 # Process payment
-                try:
-                    payment_url = process_payment(member_form.id)
-                    return redirect(payment_url) # Redirect user to JCC payment page
-                except Exception as e:
-                    messages.error(request, f"An error occurred while processing your payment: {str(e)}")
-                    return redirect('home_page/index.html')
+                # try:
+                #     payment_url = process_payment(member_form.id)
+                #     return redirect(payment_url) # Redirect user to JCC payment page
+                # except Exception as e:
+                #     messages.error(request, f"An error occurred while processing your payment: {str(e)}")
+                #     return redirect('home_page/index.html')
             else:
                 messages.error(
                     request,
