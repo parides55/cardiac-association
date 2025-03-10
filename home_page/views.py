0d79b7fc1b4e972d5663e45cddc2729484600dd8
parides55 <andreasparides5@gmail.com>
2025-03-10 01:14:09 +0000

    comment out the process payment function in become member

--- home_page/views.py
+++ home_page/views.py
@@ -28,19 +28,19 @@ def Become_member(request):
             member_form = MemberForm(request.POST)
             if member_form.is_valid():
                 member_form.save()
-                # messages.success(request, f"Welcome to the family of the Association of Children with Heart Disease." 
-                #     f"Your membership has been successfully registered.")
-                # return render(request, "home_page/index.html")
+                messages.success(request, f"Welcome to the family of the Association of Children with Heart Disease." 
+                    f"Your membership has been successfully registered.")
+                return render(request, "home_page/index.html")
                 
-                # Process payment
-                # Append a random 8-character string to the orderId to make it unique
-                unique_order_number = f"{member_form.instance.id}-{uuid.uuid4().hex[:8]}"
-                try:
-                    payment_url = process_payment(unique_order_number)
-                    return redirect(payment_url) # Redirect user to JCC payment page
-                except Exception as e:
-                    messages.error(request, f"An error occurred while processing your payment: {str(e)}")
-                    return redirect('home')
+                # # Process payment
+                # # Append a random 8-character string to the orderId to make it unique
+                # unique_order_number = f"{member_form.instance.id}-{uuid.uuid4().hex[:8]}"
+                # try:
+                #     payment_url = process_payment(unique_order_number)
+                #     return redirect(payment_url) # Redirect user to JCC payment page
+                # except Exception as e:
+                #     messages.error(request, f"An error occurred while processing your payment: {str(e)}")
+                #     return redirect('home')
             else:
                 messages.error(
                     request,
@@ -110,7 +110,6 @@ def membership_success(request, orderId):
     try:
         response = requests.post(verification_url, headers=headers, data=data)
         response_data = response.json()
-        messages.info(request, f"OrderStatus: {response_data}")
 
         if response_data.get("orderStatus") == 2:  # 2 means payment completed
             token = response_data.get("bindingId")  # Token for future payments
