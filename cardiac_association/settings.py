9dd89b1f1bb42f4d171c3a73bd24be819d9347b5
parides55 <andreasparides5@gmail.com>
2025-03-12 13:59:22 +0000

    correct a typo

--- cardiac_association/settings.py
+++ cardiac_association/settings.py
@@ -136,7 +136,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get("EMAIL_ADDRESS")
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 
 
 # Internationalization
