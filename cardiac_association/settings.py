49123f0ef955f4b11c8614a858a5de614633a15f
parides55 <andreasparides5@gmail.com>
2025-03-12 17:26:11 +0000

    ADD EMAIl_USE_SSL to false

--- cardiac_association/settings.py
+++ cardiac_association/settings.py
@@ -135,6 +135,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_ADDRESS")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = os.environ.get("EMAIL_ADDRESS")
