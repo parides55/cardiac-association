50af92b0ffb42d49a63bd675d47cf6e92fd9bfc7
parides55 <andreasparides5@gmail.com>
2025-03-12 15:31:07 +0000

    change the email settings

--- cardiac_association/settings.py
+++ cardiac_association/settings.py
@@ -134,7 +134,8 @@ AUTH_PASSWORD_VALIDATORS = [
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
 EMAIL_HOST_USER = os.environ.get("EMAIL_ADDRESS")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 
