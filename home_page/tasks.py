76bb6ec86256c23fa2828e97a7663459223b4dc1
parides55 <andreasparides5@gmail.com>
2025-03-24 17:47:25 +0000

    tempo send_email_monthly function to test beat

--- home_page/tasks.py
+++ home_page/tasks.py
@@ -1,6 +1,9 @@
+import os
+from django.core.mail import send_mail
 from celery import shared_task
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
+from django.conf import settings
 from .models import *
 
 @shared_task
@@ -14,3 +17,17 @@ def charge_annual_subscription(member_id):
     member.is_paid = True
     member.save()
     return member.id
+
+
+@shared_task(name='send_email_monthly')
+def send_email_monthly():
+    
+    subject = "Monthly Newsletter"
+    from_email = settings.EMAIL_HOST_USER
+    members = Member.objects.filter(is_paid=True)
+    
+    for member in members:
+        message = f"Dear {member.name},\n\nWe hope you are doing well. Here is our monthly newsletter. We hope you enjoy it.\n\nBest regards,\n\nThe Cardiac Association Team"
+        send_mail(subject, message, from_email, [member.email])
+
+    return f"Emails sent to {len(members)} members."
