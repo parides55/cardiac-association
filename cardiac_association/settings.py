9a460ead82e179e69165e968d928a37e778dd919
parides55 <andreasparides5@gmail.com>
2025-03-25 08:49:06 +0000

    connect the upstash redis to the project and heroku for development

--- cardiac_association/settings.py
+++ cardiac_association/settings.py
@@ -181,7 +181,8 @@ MESSAGE_TAGS = {
 }
 
 # Celery settings
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_BROKER_URL = os.environ.get("REDIS_URL") or 'redis://localhost:6379/0'   
 CELERY_BROKER_CONNECTION_RETRY_ON_STARTUP = True
 CELERY_RESULT_BACKEND = 'django-db'
 CELERY_RESULT_EXTENDED = True
+CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers.DatabaseScheduler'
