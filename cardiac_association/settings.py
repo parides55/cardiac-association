291dc9404183424b552d18511b4f646b61b60065
parides55 <andreasparides5@gmail.com>
2025-03-25 09:43:36 +0000

    update settings.py with CELERY_BROKER_TRANSPORT_OPTIONS

--- cardiac_association/settings.py
+++ cardiac_association/settings.py
@@ -182,6 +182,7 @@ MESSAGE_TAGS = {
 
 # Celery settings
 CELERY_BROKER_URL = os.environ.get("REDIS_URL", 'rediss://localhost:6379/0')
+CELERY_BROKER_TRANSPORT_OPTIONS = {"ssl": {"ssl_cert_reqs": "CERT_NONE"}}  # Needed for Upstash Redis
 CELERY_BROKER_CONNECTION_RETRY_ON_STARTUP = True
 CELERY_RESULT_BACKEND = os.environ.get("REDIS_URL")
 CELERY_RESULT_EXTENDED = True
