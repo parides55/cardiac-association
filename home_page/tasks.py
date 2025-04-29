0168be2fd713640f48a50ecb350ee746796af40c
parides55 <andreasparides5@gmail.com>
2025-04-29 23:21:58 +0300

    simplify the function for testing

--- home_page/tasks.py
+++ home_page/tasks.py
@@ -11,53 +11,42 @@ def check_member_for_renewal():
     
     today = datetime.today().date()
     
-    for member in Member.objects.all():
-        if member.membership_status == 'active' and member.next_payment_date == today:
-            member_client_id = member.client_id
-            
-            try:
-                stored_credentials = get_credentials(member_client_id)
-                
-                subject = "Membership Renewal"
-                
-                text_content = f"""
-                The membership for {member.name} {member.surname} is due for renewal.
-                
-                These are the stored credentials:
-                {stored_credentials}
-                """
-                
-                mail_admins(subject, text_content)
-            
-            except Exception as e:
-                error_message = f"Error checking member {member.name} {member.surname}: {str(e)}"
-                mail_admins("Error in Membership Renewal Check", error_message)
-
-
-def get_credentials(client_id):
-
-    url = "https://gateway-test.jcc.com.cy/payment/rest/getBindings.do"
-    headers = {"Content-Type": "application/x-www-form-urlencoded"}
+    members_for_renewal = Member.objects.filter(membership_expiry_date__lte=today,)
     
-    data ={
-        "userName": settings.JCC_USERNAME,
-        "password": settings.JCC_PASSWORD,
-        "clientId": client_id,
-    }
+    subject = "Membership Expiry Reminder"
     
-    try:
-        response = requests.post(url, headers=headers, data=data)
-        response_data = response.json()
+    text_content = f""""
+    Tme members for renewal are :
+    
+    {members_for_renewal}
+    """           
+
+    mail_admins(subject, text_content)
+
+# def get_credentials(client_id):
+
+#     url = "https://gateway-test.jcc.com.cy/payment/rest/getBindings.do"
+#     headers = {"Content-Type": "application/x-www-form-urlencoded"}
+    
+#     data ={
+#         "userName": settings.JCC_USERNAME,
+#         "password": settings.JCC_PASSWORD,
+#         "clientId": client_id,
+#     }
+    
+#     try:
+#         response = requests.post(url, headers=headers, data=data)
+#         response_data = response.json()
         
-        if response_data.get("errorCode") == "0":
-            binding_id = response_data.get("bindings", [])
-            return binding_id
-        else:
-            error_message = response_data.get("errorMessage")
-            return error_message
+#         if response_data.get("errorCode") == "0":
+#             binding_id = response_data.get("bindings", [])
+#             return binding_id
+#         else:
+#             error_message = response_data.get("errorMessage")
+#             return error_message
 
-    except Exception as e:
-        return str(e)
+#     except Exception as e:
+#         return str(e)
 
 
     
