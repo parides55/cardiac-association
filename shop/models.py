1fe39b23a62a92ea7b5c5b707b7c8126f12f63d5
parides55 <andreasparides5@gmail.com>
2025-05-16 12:47:42 +0300

    add 3 fields in donation model, client_id, last payment_date, next_payment_date

--- shop/models.py
+++ shop/models.py
@@ -109,7 +109,9 @@ class Donation(models.Model):
     status = models.CharField(max_length=20, choices=STATUS_CHOICES)
     is_paid = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    last_payment_date = models.DateTimeField(blank=True, null=True)
+    next_payment_date = models.DateTimeField(blank=True, null=True)
+    client_id = models.CharField(max_length=100, blank=True, null=True)
     
     
     def __str__(self):
