2e518cd9d0efadc58fe11e3deb94ef848b9e37d9
parides55 <andreasparides5@gmail.com>
2025-06-04 14:56:04 +0300

    add video option in the admins gallery

--- about_us/admin.py
+++ about_us/admin.py
@@ -1,20 +1,24 @@
 from django.contrib import admin
 from .models import People
 from django.contrib import admin
-from .models import Gallery, GalleryImage
+from .models import Gallery, GalleryImage, GalleryVideo
 
 
 class GalleryImageInline(admin.TabularInline):
     model = GalleryImage
     extra = 1  # Allows adding multiple images at once
+    
+
+class GalleryVideoInline(admin.TabularInline):
+    model = GalleryVideo
+    extra = 1  # Allows adding multiple videos at once
 
 
 @admin.register(Gallery)
 class GalleryAdmin(admin.ModelAdmin):
-    inlines = [GalleryImageInline]
+    inlines = [GalleryImageInline, GalleryVideoInline]
     list_display = ("title", "created_at")
 
 
 # Register your models here.
 admin.site.register(People)
-admin.site.register(GalleryImage)
