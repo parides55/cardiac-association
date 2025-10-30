32dde93cf7f63bbb2ca25c2682d86712dc516d99
parides55 <andreasparides5@gmail.com>
2025-10-30 11:46:29 +0200

    add new fields for greek content and migrate it

--- events/models.py
+++ events/models.py
@@ -7,17 +7,20 @@ STATUS = ((0, "Draft"), (1, "Published"))
 
 class Event(models.Model):
     
-    title = models.CharField(max_length=200, unique=True)
+    title_english = models.CharField(max_length=200, unique=True)
+    title_greek = models.CharField(max_length=200, default='update in greek')
     slug = models.SlugField(max_length=200, unique=True)
     image = CloudinaryField('image', default='placeholder')
-    content = models.TextField()
+    content_english = models.TextField()
+    content_greek = models.TextField(default='update in greek')
     date_of_event = models.DateTimeField()
     created_on = models.DateTimeField(auto_now_add=True)
-    location = models.CharField(max_length=200)
+    location_english = models.CharField(max_length=200)
+    location_greek = models.CharField(max_length=200, default='update in greek')
     status = models.IntegerField(choices=STATUS, default=0)
     
     def __str__(self):
-        return f"{self.title} - {self.created_on.strftime('%Y-%m-%d')}"
+        return f"{self.title_greek} - {self.created_on.strftime('%Y-%m-%d')}"
     
     class Meta:
         ordering = ['-created_on']
