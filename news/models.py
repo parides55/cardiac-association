15fb6c5377f0873f65e8aa1acd62ebbfc6df6cb7
parides55 <andreasparides5@gmail.com>
2025-10-30 11:04:03 +0200

    add more fields to include the greek and english content and migrate it

--- news/models.py
+++ news/models.py
@@ -7,17 +7,20 @@ STATUS = ((0, "Draft"), (1, "Published"))
 
 
 class New(models.Model):
-    title = models.CharField(max_length=200, unique=True)
+    title_english = models.CharField(max_length=200, unique=True)
+    title_greek = models.CharField(max_length=200, default='update in greek')
     slug = models.CharField(max_length=200, unique=True)
     image = CloudinaryField('image', default='placeholder')
-    content = models.TextField()
+    content_english = models.TextField()
+    content_greek = models.TextField(default='update in greek')
     created_on = models.DateTimeField(auto_now_add=True)
-    excerpt = models.TextField(blank=True)
+    excerpt_english = models.TextField(blank=True)
+    excerpt_greek = models.TextField(blank=True, default='update in greek')
     status = models.IntegerField(choices=STATUS, default=0)
     
     
     def __str__(self):
-        return f"{self.title} - {self.created_on.strftime('%Y-%m-%d')}"
+        return f"{self.title_greek} - {self.created_on.strftime('%Y-%m-%d')}"
     
     class Meta:
         ordering = ['-created_on']
