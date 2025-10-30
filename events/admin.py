831351247ac74f7cb5e908584c18a8e90e8e285e
parides55 <andreasparides5@gmail.com>
2025-10-30 11:45:11 +0200

    update the names of the fields

--- events/admin.py
+++ events/admin.py
@@ -6,10 +6,10 @@ from .models import Event
 @admin.register(Event)
 class EventAdmin(SummernoteModelAdmin):
 
-    list_display = ('title', 'created_on', 'date_of_event', 'status')
+    list_display = ('title_greek', 'created_on', 'date_of_event', 'status')
     list_filter = ('created_on', 'status')
-    search_fields = ['title', 'created_on']
-    prepopulated_fields = {'slug': ('title',)}
-    summernote_fields = ('content',)
+    search_fields = ['title_greek', 'created_on']
+    prepopulated_fields = {'slug': ('title_english',)}
+    summernote_fields = ('content_english', 'content_greek')
 
 # Register your models here.
