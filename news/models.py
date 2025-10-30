44540577925f1782ceaefe0322a437e10877b01b
parides55 <andreasparides5@gmail.com>
2025-10-30 11:23:44 +0200

    update the values of the greek fields, after updating the greek content in the model

--- news/models.py
+++ news/models.py
@@ -8,14 +8,14 @@ STATUS = ((0, "Draft"), (1, "Published"))
 
 class New(models.Model):
     title_english = models.CharField(max_length=200, unique=True)
-    title_greek = models.CharField(max_length=200, default='update in greek')
+    title_greek = models.CharField(max_length=200, unique=True)
     slug = models.CharField(max_length=200, unique=True)
     image = CloudinaryField('image', default='placeholder')
     content_english = models.TextField()
-    content_greek = models.TextField(default='update in greek')
+    content_greek = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     excerpt_english = models.TextField(blank=True)
-    excerpt_greek = models.TextField(blank=True, default='update in greek')
+    excerpt_greek = models.TextField(blank=True)
     status = models.IntegerField(choices=STATUS, default=0)
     
     
