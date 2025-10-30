3c524bd490aa3d59c82d295a71a1f9ea64af99fc
parides55 <andreasparides5@gmail.com>
2025-10-30 13:28:55 +0200

    add to the list_display the is_paid field

--- home_page/admin.py
+++ home_page/admin.py
@@ -6,7 +6,7 @@ from .models import Member
 @admin.register(Member)
 class MemberAdmin(SummernoteModelAdmin):
     
-    list_display = ('id', 'name', 'surname', 'date_of_birth', 'membership_status', 'created_at')
+    list_display = ('id', 'name', 'surname', 'is_paid', 'membership_status', 'created_at')
     list_filter = ('date_of_birth', 'name', 'surname', 'membership_status')
     search_fields = ['name', 'surname', 'email', 'id_number', 'date_of_birth']
 
