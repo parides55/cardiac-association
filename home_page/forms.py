03bfa20cde09b5c31e1b96b61e231bd447d5d983
Andreas Parides <parides5@hotmail.com>
2026-06-11 18:43:14 +0300

    add ReCaptchField widget V2 checkbox

--- home_page/forms.py
+++ home_page/forms.py
@@ -1,8 +1,13 @@
 from django import forms
 from django.utils.translation import gettext_lazy as _
+from django_recaptcha.fields import ReCaptchaField
+from django_recaptcha.widgets import ReCaptchaV2Checkbox
 from .models import Member
 
 class MemberForm(forms.ModelForm):
+    
+    captcha = ReCaptchaField(widget=ReCaptchaV2Checkbox)
+
     class Meta:
         model = Member
         fields = [
