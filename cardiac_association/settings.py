705b4272bdb6c5df6f6c917f0d25440e714a29f4
Andreas Parides <parides5@hotmail.com>
2026-06-11 18:36:31 +0300

    correct the string for key variables in settings.py

--- cardiac_association/settings.py
+++ cardiac_association/settings.py
@@ -156,8 +156,8 @@ SERVER_EMAIL = os.environ.get("SERVER_EMAIL")
 ACCOUNTANT_EMAIL = os.environ.get("ACCOUNTANT_EMAIL")
 
 # reCAPTCHA settings
-RECAPTCHA_PUBLIC_KEY = os.environ.get("RECAPTCHA_PUBLIC_KEY_V2")
-RECAPTCHA_PRIVATE_KEY = os.environ.get("RECAPTCHA_PRIVATE_KEY_V2")
+RECAPTCHA_PUBLIC_KEY = os.environ.get("RECAPTCHA_PUBLIC_KEY")
+RECAPTCHA_PRIVATE_KEY = os.environ.get("RECAPTCHA_PRIVATE_KEY")
 SILENCED_SYSTEM_CHECKS = ['django_recaptcha.recaptcha_test_key_error']
 
 # Internationalization
